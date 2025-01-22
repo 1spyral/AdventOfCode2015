2d23d566ac461c7d4b53df799b920927f7946702
1spyral <83098914+1spyral@users.noreply.github.com>
2025-01-21 20:32:30 -0500

    Solve Day 4 Problem 2
    What the hell...

---
+++ day04/p2.cpp
@@ -0,0 +1,44 @@
+// AoC 2015 Day 4 Problem 2
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "md5.cpp"
+
+using namespace std;
+
+bool check(string str) {
+    for (int i = 0; i < 6; i++) {
+        if (str[i] != '0') {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    ifstream input("input.txt");
+
+    string line;
+    getline(input, line);
+
+    int i = 1;
+
+    while (true) {
+        string str = line + to_string(i);
+
+        void* sig = hashing::md5::hash(str);
+
+        if (check(hashing::md5::sig2hex(sig))) {
+            break;
+        }
+
+        i++;
+    }
+
+    cout << i;
+
+    input.close();
+    return 0;
+}
