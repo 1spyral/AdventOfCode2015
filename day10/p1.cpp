b9553e7de338a225cb37b3ac84a5a714cb94eb21
Luke Zhan <luke.zhan@uwaterloo.ca>
2025-01-23 22:58:13 -0500

    Solve Day 10 Problem 1
    So I wasn't at my main computer today. So I used OnlineGDB. Which doesn't support the input.txt file. But luckily this question's input is a single small line. Lucky!

---
+++ day10/p1.cpp
@@ -0,0 +1,42 @@
+// AoC 2015 Day 10 Problem 1
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string lookandsay(string input) {
+    int count = 1;
+    char prev = input[0];
+    
+    string out = "";
+    
+    for (int i = 1; i < input.length(); i++) {
+        if (input[i] == prev) {
+            count++;
+        } else {
+            out += to_string(count);
+            out += prev;
+            count = 1;
+            prev = input[i];
+            }
+    }
+    
+    out += to_string(count);
+    out += prev;
+    
+    return out;
+}
+
+int main() {
+    string str = "1113122113";
+    
+    for (int i = 0; i < 40; i++) {
+        str = lookandsay(str);
+    }
+    
+    cout << str.length();
+
+    return 0;
+}
