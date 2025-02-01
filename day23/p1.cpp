668eefff99326455ef599c78dc6182a99640f85f
1spyral <83098914+1spyral@users.noreply.github.com>
2025-02-01 14:04:49 -0500

    Solve Day 23 Problem 1

---
+++ day23/p1.cpp
@@ -0,0 +1,71 @@
+// AoC 2015 Day 23 Problem 1
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+#define OPERATIONS 46
+
+using namespace std;
+
+int main() {
+    ifstream input("input.txt");
+
+    unsigned int a = 0;
+    unsigned int b = 0;
+
+    string operations[OPERATIONS];
+    char reg[OPERATIONS];
+    int offset[OPERATIONS];
+
+    string word;
+    for (int i = 0; i < OPERATIONS; i++) {
+        getline(input, word, ' ');
+        operations[i] = word;
+        if (word == "jie" || word == "jio") {
+            getline(input, word, ' ');
+            reg[i] = word[0];
+            getline(input, word);
+            offset[i] = stoi(word);
+            continue;
+        }
+        getline(input, word);
+        if (operations[i] == "jmp") {
+            offset[i] = stoi(word);
+            continue;
+        }
+        reg[i] = word[0];
+    }
+
+    int index = 0;
+
+    while (index < OPERATIONS && index >= 0) {
+        unsigned int* p;
+        if (reg[index] == 'a') {
+            p = &a;
+        } else {
+            p = &b;
+        }
+        if (operations[index] == "hlf") {
+            *p >>= 1;
+        } else if (operations[index] == "tpl") {
+            *p *= 3;
+        } else if (operations[index] == "inc") {
+            (*p)++;
+        } else if (
+            operations[index] == "jmp" ||
+            operations[index] == "jie" && *p % 2 == 0 ||
+            operations[index] == "jio" && *p == 1
+            ) {
+            index += offset[index];
+            continue;
+        }
+        index++;
+    }
+
+    cout << b;
+
+    input.close();
+    return 0;
+}
